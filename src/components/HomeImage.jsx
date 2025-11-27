5edbc1f72855c275c94582f98a037800f8c32a43
aslamkhanpatha142 <khanaslampathan142@gmail.com>
2025-11-27 21:33:07 +0530

    Home page Image chang

--- src/components/HomeImage.jsx
+++ src/components/HomeImage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "../styles/HomeImage.module.css";
+import home1 from '/home1.png'
 const HomeImage = () => {
   return (
     <>
       <div className={styles.img1}>
+        <div className={styles.insideText}>
         <p className={styles.heading}>Color of Summer Outfit</p>
         <p className={styles.para}>100+ Collections for your outfit inspirations in this summer</p>
         <button>VIEW COLLECTIONS</button>
+        </div>
+        <img src={home1} alt="" />
       </div>
     </>
   );
